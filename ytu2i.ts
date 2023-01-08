8fe4903bceb6348777160118a0aeda477083215c
ryoo14 <anana12185@gmail.com>
2023-01-08 16:38:57 +0900

    docs: add comments in deno doc format

--- ytu2i.ts
+++ ytu2i.ts
@@ -5,20 +5,37 @@ if (Deno.args.length !== 1) {
   Deno.exit(1);
 }
 
-// only support "https://www.youtube.com/@username"
-// TODO: validate user input
-const url = Deno.args[0];
-
 try {
+  /** 
+   * The channel URL of the user whose channel id want to extract.
+   * only support `https://www.youtube.com/@handle`
+   * TODO: validate user input
+   */
+  const url = Deno.args[0];
   const res = await fetch(url);
   const html = await res.text();
   const doc = new DOMParser().parseFromString(html, "text/html");
+  if (doc == null) {
+    throw new Error("variable doc is null.");
+  }
 
-  // channel_id is contained within the element of the element below.
-  // <link rel="alternate" type="application/rss+xml" title="RSS" href="https://www.youtube.com/feeds/videos.xml?channel_id=xxxx">
+  /**
+   * URL include channel id. 
+   * It is included in the link tag like the example in.
+   * <link rel="alternate" type="application/rss+xml" title="RSS" href="https://www.youtube.com/feeds/videos.xml?channel_id=xxxx">
+   */
   const channelIdUrl = doc.querySelector("link[title='RSS']")?.getAttribute("href");
 
-  console.log(channelIdUrl.split("=")[1]);
+  if (channelIdUrl == null) {
+    throw new Error("variable channelIdUrl is null or undefined.");
+  }
+
+  /**
+   * channel id.
+   */
+  const channelId = channelIdUrl.split("=")[1];
+
+  console.log(channelId);
 } catch (e) {
   console.error(e.message);
 }
