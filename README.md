53d87691ffdc24fed668eb49a050405746fdbb38
ryoo14 <anana12185@gmail.com>
2024-01-24 22:28:07 +0900

    chore: change sample channel

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ YTU2I is a CLI tool to identify channel id from YouTube username(handle).
 ## Usage
 
 ```sh
-$ ytu2i https://www.youtube.com/@InugamiKorone # InugamiKorone is lovely VTuber in Japan :)
-UChAnqc_AY5_I3Px5dig3X1Q
+$ ytu2i https://www.youtube.com/@AmaneKanata # AmaneKanata is lovely VTuber in Japan :)
+UCZlDXzGoo7d44bwdNObFacg
 ```
--- ytu2i_test.ts
+++ ytu2i_test.ts
@@ -2,6 +2,6 @@ import { assertEquals } from "https://deno.land/std@0.211.0/assert/mod.ts";
 import $ from "https://deno.land/x/dax@0.36.0/mod.ts";
 
 Deno.test("convert userName to channelID", async () => {
-  const result = await $`deno run --allow-net ytu2i.ts https://www.youtube.com/@GoogleJapan`.text();
-  assertEquals(result, "UCTnRxVUPssiL7H29ON07t_Q");
+  const result = await $`deno run --allow-net ytu2i.ts https://www.youtube.com/@AmaneKanata`.text();
+  assertEquals(result, "UCZlDXzGoo7d44bwdNObFacg");
 });
