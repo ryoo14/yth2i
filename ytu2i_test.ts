8dc546e0e773b75b14d30c66f5d9eed21c99a3c7
ryoo14 <anana12185@gmail.com>
2024-03-21 11:58:39 +0900

    build: bump the modules version

--- ytu2i_test.ts
+++ ytu2i_test.ts
@@ -1,5 +1,5 @@
-import { assertEquals } from "https://deno.land/std@0.211.0/assert/mod.ts"
-import $ from "https://deno.land/x/dax@0.36.0/mod.ts"
+import { assertEquals } from "https://deno.land/std@0.220.1/assert/mod.ts"
+import $ from "https://deno.land/x/dax@0.39.2/mod.ts"
 
 Deno.test("convert userName to channelID", async () => {
   const result = await $`deno run --allow-net ytu2i.ts https://www.youtube.com/@AmaneKanata`.text()
