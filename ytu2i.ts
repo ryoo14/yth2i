3b67c44f68561fda1eb5262a0926eab556c7d225
ryoo14 <anana12185@gmail.com>
2024-05-11 15:49:43 +0900

    style: consolidate the library version entries in deno.json
    - close #5

--- ytu2i.ts
+++ ytu2i.ts
@@ -1,4 +1,4 @@
-import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.45/deno-dom-wasm.ts"
+import { DOMParser } from "deno_dom"
 
 if (Deno.args.length !== 1) {
   console.error("Pleases input only one args.")
--- ytu2i_test.ts
+++ ytu2i_test.ts
@@ -1,5 +1,5 @@
-import { assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts"
-import $ from "https://deno.land/x/dax@0.39.2/mod.ts"
+import { assertEquals } from "std/assert"
+import $ from "dax"
 
 Deno.test("convert userName to channelID", async () => {
   const result = await $`deno run --allow-net ytu2i.ts https://www.youtube.com/@AmaneKanata`.text()
