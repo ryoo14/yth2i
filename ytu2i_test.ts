ee8eb4623b05ff983475ede4e502420ab33ceb42
ryoo14 <anana12185@gmail.com>
2024-05-11 16:03:18 +0900

    build: migrate jsr
    - close #4

--- ytu2i_test.ts
+++ ytu2i_test.ts
@@ -1,5 +1,5 @@
-import { assertEquals } from "std/assert"
-import $ from "dax"
+import { assertEquals } from "@std/assert"
+import $ from "@david/dax"
 
 Deno.test("convert userName to channelID", async () => {
   const result = await $`deno run --allow-net ytu2i.ts https://www.youtube.com/@AmaneKanata`.text()
